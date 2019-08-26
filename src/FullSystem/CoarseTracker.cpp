808c96efa5778842ad8cbb0ab0ed1660651d4126
rancheng <chengran327@gmail.com>
2019-08-25 23:11:13 -0400

    cumulated H and b

--- src/FullSystem/CoarseTracker.cpp
+++ src/FullSystem/CoarseTracker.cpp
@@ -334,10 +334,10 @@ void CoarseTracker::calcGSSSE(int lvl, Mat88 &H_out, Vec8 &b_out, const SE3 &ref
 				_mm_load_ps(buf_warped_weight+i)); // weight[i]
 	}
 
-	acc.finish(); // finish accumulator
+	acc.finish(); // loop all the buffer and cumulate into H and b
 	H_out = acc.H.topLeftCorner<8,8>().cast<double>() * (1.0f/n);
 	b_out = acc.H.topRightCorner<8,1>().cast<double>() * (1.0f/n);
-
+    // scale H and b.
 	H_out.block<8,3>(0,0) *= SCALE_XI_ROT;
 	H_out.block<8,3>(0,3) *= SCALE_XI_TRANS;
 	H_out.block<8,1>(0,6) *= SCALE_A;
