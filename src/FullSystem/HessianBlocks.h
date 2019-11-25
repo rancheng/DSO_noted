bf1dfdaaa0fd791e2b701375d0848e821e8ca2d6
ran <752050943@qq.com>
2019-11-25 18:09:34 -0500

    correct some notation in the paper

--- src/FullSystem/HessianBlocks.h
+++ src/FullSystem/HessianBlocks.h
@@ -218,7 +218,7 @@ struct FrameHessian
 	};
 
 
-
+    // invoked in CoarseInitializer
 	inline void setEvalPT_scaled(const SE3 &worldToCam_evalPT, const AffLight &aff_g2l)
 	{
 		Vec10 initial_state = Vec10::Zero();
